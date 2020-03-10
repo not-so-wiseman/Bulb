966a12ac0d524b791dab71de7a86e846404ca0d3
Christian James <csjjames@mun.ca>
2020-03-10 13:58:50 -0230

    Base Template For New Empty Activity

--- README.md
+++
@@ -1,9 +0,0 @@
-# Bulb
-A companion app for Brightspace
-
-## Requirements
-* View grade breakdown
-* Get push notification when new assignments or grades have been released
-* Auto-updated Calendar (Midterms/Quizzes/Assignments)
-
-
--- RestAPI/src/LampAPI/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# To do: 
-# replace values with environment variables
-
-host = 'muntest.brightspace.com' #os.environ['D2L_SERVER']
-target = 'http://blub.tech' #os.environ['API_URL']
-app_id = 'rumLdUKs14G5okboyjFkmQ' #os.environ['APP_ID']
-app_key = 'UL_WoG-mxZNO_vgS0ooGQg' #os.environ['APP_KEY']
