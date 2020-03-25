8fad8d48d69689b48424974c9ea366f3a36f9a0e
ewiseman <ewiseman@mun.ca>
2020-03-25 19:49:12 -0230

    fixed bug

--- RestAPI/src/app.py
+++ RestAPI/src/app.py
@@ -42,7 +42,7 @@ def courses(api):
     try:
         courses_json = []
         for course in api.courses():
-            courses_json.append(course)
+            courses_json.append(str(course))
         return str(courses_json)
     except Exception as e:
         return str("[{}] {}".format(500,e))
