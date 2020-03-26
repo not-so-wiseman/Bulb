5726f21d96b3319d1bc9cc440babf21a7b282c58
ewiseman <ewiseman@mun.ca>
2020-03-26 03:32:48 -0230

    debugging

--- RestAPI/src/LampAPI/lamp.py
+++ RestAPI/src/LampAPI/lamp.py
@@ -94,17 +94,16 @@ class Lamp:
         return self._return_grade_info(course_no).categorized_items
 
     def grades_all(self):
-        
         grades_json = []
         courses = self._return_course_info()
-        """
+        
         for course in courses.data:
             grade_info = self._return_grade_info(course.id)
             course.json["Grades"] = grade_info.categorized_items
             course.json["Average"] = grade_info.average()
             grades_json.append(deepcopy(course.json))
-        """
-        return json.dumps(courses.data)
+        
+        return json.dumps(grades_json)
             
    
 
