4d278c6fed9598dc4e98ff1ac5e0f14c29586a04
ewiseman <ewiseman@mun.ca>
2020-03-26 02:34:57 -0230

    fixed bug

--- RestAPI/src/LampAPI/utils/courses.py
+++ RestAPI/src/LampAPI/utils/courses.py
@@ -27,12 +27,15 @@ class Courses:
             details = course["Access"]
             end_date = details["EndDate"]
 
+            """
             if(details["CanAccess"] == True and details["EndDate"] != None):
                 end_date = datetime.strptime(end_date, '%Y-%m-%dT%H:%M:%S.%fZ')
                 delta = end_date - datetime.now()
                 
-                #if(delta.total_seconds() > 0):
-                active_courses.append(Course(course))
+                if(delta.total_seconds() > 0):
+                    active_courses.append(Course(course))
+            """
+            active_courses.append(Course(course))
         return active_courses
 
     
