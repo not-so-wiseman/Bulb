73d8e85f413a3a6bec83ef23c81b377fc8611cce
ewiseman <ewiseman@mun.ca>
2020-03-30 19:54:53 -0230

    added test

--- RestAPI/src/LampAPI/lamp.py
+++ RestAPI/src/LampAPI/lamp.py
@@ -127,11 +127,12 @@ class Lamp:
             3065118
         )
         request = self._get(route)
-        #pdfReader = PyPDF2.PdfFileReader(request.text)
-        #pageObj = pdfReader.getPage(0)
+        pdf = io.BytesIO(request.text)
+        pdfReader = PyPDF2.PdfFileReader(pdf)
+        pageObj = pdfReader.getPage(0)
 
-        #return str(pageObj.extractText())
-        return request.text
+        return str(pageObj.extractText())
+        #return request.text
 
 
 
