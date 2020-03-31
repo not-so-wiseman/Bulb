8a904d40e7ba443453ba36262fba229b54ec6037
ewiseman <ewiseman@mun.ca>
2020-03-31 15:47:06 -0230

    test

--- RestAPI/src/LampAPI/utils/calendar.py
+++ RestAPI/src/LampAPI/utils/calendar.py
@@ -2,7 +2,17 @@ import tika
 from tika import parser
 
 class Calendar:
+    attempt_no = 0
+
     def __init__(self, pdf_stream):
-        self.pdf = str(parser.from_buffer(pdf_stream)['content'])
+        try:
+            self.pdf = str(parser.from_buffer(pdf_stream)['content'])
+        except RuntimeError:
+            self.attempt_no += 1
+            if self.attempt_no != 10:
+                print("Attempt: {}\n".format(self.attempt_no))
+                self.__init__(pdf_stream)
+            else:
+                print("Failed to connect to Tika server")
 
     
